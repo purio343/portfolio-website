3e13836efaf72b396e839baea08a877717ef2e47
purio <wizard.bass5@gmail.com>
2025-08-01 17:13:24 +0900

    スキルセクションを動的に生成する機能を追加し、ハードコーディングされたスキルを削除

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,23 @@ function embeddingInformation() {
       document.getElementById("title").innerHTML = data.title;
       document.getElementById("name").innerHTML = data.name;
       document.getElementById("email").innerHTML = data.email;
+      const skills = data.skills;
+      addSkills(skills);
     });
+}
+
+function addSkills(skills) {
+  const skillsContainer = document.getElementById("skills");
+  skills.forEach((skill) => {
+    const div = document.createElement("div");
+    div.className =
+      "flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#e7edf4] pl-4 pr-4";
+    const p = document.createElement("p");
+    p.className = "text-[#0d141c] text-sm font-medium leading-normal";
+    div.appendChild(p);
+    p.innerHTML = skill;
+    skillsContainer.appendChild(div);
+  });
 }
 
 embeddingInformation();
